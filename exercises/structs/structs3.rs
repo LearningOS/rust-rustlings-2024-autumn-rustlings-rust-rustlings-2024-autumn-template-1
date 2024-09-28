b0ff3767d9dff1a0b64a3e69249199e8af679ae0
Alan Young <823059911@qq.com>
2024-09-28 22:31:19 +0800

    update

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,12 +29,19 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
+        if self.sender_country != self.recipient_country{
+            true
+        }else {
+            false
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+         cents_per_gram*self.weight_in_grams
+
     }
 }
 
