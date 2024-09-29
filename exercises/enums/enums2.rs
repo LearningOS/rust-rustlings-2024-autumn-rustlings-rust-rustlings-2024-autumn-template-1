8babf8db1dabc2c6aac8c40b815dfd7caafcb04f
HaitianMu <110078522+HaitianMu@users.noreply.github.com>
2024-09-29 09:03:21 +0000

    modified:   exercises/enums/README.md 	modified:   exercises/enums/enums1.rs 	modified:   exercises/enums/enums2.rs

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,11 +3,17 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Quit,
+    ChangeColor(u32,u32,u32),
+    //i32：有符号整数（signed integer）
+    //u32：无符号整数（unsigned integer）
+    Move{x:i32,y:i32},
+    Echo(String)
 }
 
 impl Message {
