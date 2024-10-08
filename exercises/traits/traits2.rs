6487db22d14a54194e509a0bda160054b487e0ef
HaitianMu <110078522+HaitianMu@users.noreply.github.com>
2024-10-08 07:54:04 +0000

    modified:   exercises/generics/generics1.rs 	modified:   exercises/generics/generics2.rs 	modified:   exercises/lifetimes/lifetimes1.rs 	modified:   exercises/lifetimes/lifetimes2.rs 	modified:   exercises/lifetimes/lifetimes3.rs 	modified:   exercises/quiz3.rs 	modified:   exercises/tests/tests1.rs 	modified:   exercises/tests/tests2.rs 	modified:   exercises/tests/tests3.rs 	modified:   exercises/tests/tests4.rs 	modified:   exercises/tests/tests5.rs 	modified:   exercises/traits/traits1.rs 	modified:   exercises/traits/traits2.rs 	modified:   exercises/traits/traits3.rs# 	modified:   exercises/traits/traits4.rs 	modified:   exercises/traits/traits5.rs

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,12 +8,18 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push(String::from("Bar"));
+        self
+    }
+}
 // TODO: Implement trait `AppendBar` for a vector of strings.
 
 #[cfg(test)]
