4fb3ee47750ff167da098f77998436b1032c78e7
lsmx0 <1286962598@qq.com>
2024-10-10 21:32:10 +0800

    modified:   exercises/lifetimes/lifetimes1.rs 	modified:   exercises/lifetimes/lifetimes2.rs 	modified:   exercises/lifetimes/lifetimes3.rs 	modified:   exercises/tests/tests1.rs 	modified:   exercises/tests/tests2.rs

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -6,10 +6,10 @@
 // Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
+
+fn longest (x: String, y: String) -> String {
+    if &x.len() > &y.len() {
         x
     } else {
         y
@@ -20,8 +20,9 @@ fn main() {
     let string1 = String::from("long string is long");
     let result;
     {
+        
         let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
+        result = longest(string1, string2);
     }
-    println!("The longest string is '{}'", result);
+    println!("The longest string is {}", result);
 }
